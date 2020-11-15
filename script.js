d8abff2f9825eb35f011c3f7a51368413acfd8f3
Kevin M. Smith <smithkevin1100@gmail.com>
2020-11-15 17:06:32 +0000

    Updating repo

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let genres = ['Aquaman', 'Batgirl', 'Batman', 'Black Panther', 'Captain America', 'Doctor Strange', 'Ghost Rider', 'Spider-Man' ];
+let movedGenres = [];
+const init = () => {
+  generateGenres()
+  document.getElementById("form").addEventListener("submit", validate);
+  console.log({genres});
+  console.log({movedGenres});
+}
 const validate = (e) => {
   let valid = true;
   let inputs = document.querySelectorAll(".yoke");
@@ -23,4 +31,54 @@ let checkDoesPass = checkboxArray.every(checkboxTest);
     return true;
   }
 };
-document.getElementById("form").addEventListener("submit", validate);
+const moveGenre = (element) =>{
+  console.log({genres});
+  //element.style.backgroundColor = 'blue';
+  //document.getElementById("genreSecond").appendChild(element);
+  if(genres.includes(element.innerHTML)){
+  const moving = genres.indexOf(element.innerHTML)
+  movedGenres.push(genres[moving]);
+  genres.splice(moving, 1);
+  }else{
+    const moving = movedGenres.indexOf(element.innerHTML)
+  genres.push(movedGenres[moving]);
+  movedGenres.splice(moving, 1);
+  }
+  console.log({genres});
+  console.log({movedGenres});
+  genres.sort();
+  movedGenres.sort();
+  generateGenres();
+
+
+}
+const generateGenres = () => {
+  document.getElementById("genreFirst").innerHTML = '';
+  document.getElementById("genreSecond").innerHTML = '';
+  for (let i = 0; i < genres.length; i++) {
+    //document.getElementById('genreFirst').innerHTML += genres[i];
+    const genre = document.createElement('div');
+    genre.setAttribute('class', 'genre');
+    genre.setAttribute('onclick', 'moveGenre(this)');
+
+    const textnode = document.createTextNode(genres[i]);       
+    genre.appendChild(textnode);                         
+    document.getElementById("genreFirst").appendChild(genre);
+    
+  }
+  for (let i = 0; i < movedGenres.length; i++) {
+    //document.getElementById('genreFirst').innerHTML += genres[i];
+    const genre = document.createElement('div');
+    genre.setAttribute('class', 'genre');
+    genre.setAttribute('onclick', 'moveGenre(this)');
+
+    const textnode = document.createTextNode(movedGenres[i]);       
+    genre.appendChild(textnode);                         
+    document.getElementById("genreSecond").appendChild(genre);
+    
+  }
+  document.getElementById('genreSecond').value = movedGenres;
+}
+
+
+init()
