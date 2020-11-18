e861b3a58d1b907d5621ba867c515c5438f02bf1
Kevin M. Smith <smithkevin1100@gmail.com>
2020-11-18 13:36:51 +0000

    Added Submission and Errors

--- script.js
+++ script.js
@@ -1,35 +1,81 @@
 let genres = ['Aquaman', 'Batgirl', 'Batman', 'Black Panther', 'Captain America', 'Doctor Strange', 'Ghost Rider', 'Spider-Man' ];
 let movedGenres = [];
-const init = () => {
-  generateGenres()
-  document.getElementById("form").addEventListener("submit", validate);
-  console.log({genres});
-  console.log({movedGenres});
-}
-const validate = (e) => {
-  let valid = true;
-  let inputs = document.querySelectorAll(".yoke");
-  
-    let checkboxes = document.querySelectorAll('.checkbox');
+const images = document.getElementsByClassName('verify-image')
+let find = Math.ceil(Math.random() * 4);
+let correctImage = false;
+let valid = true;
+/*let checkboxes = document.querySelectorAll('.checkbox');
 const checkboxArray = Array.apply(null, checkboxes);
 const checkboxTest = box => box.checked;
-let checkDoesPass = checkboxArray.every(checkboxTest);
+let checkDoesPass = checkboxArray.every(checkboxTest);*/
+let checkDoesPass = false;
+const init = () => {
+  generateGenres()
+  //document.getElementById("form").addEventListener("submit", validate);
+  console.log({genres});
+  console.log({movedGenres});
+
+  find = Math.ceil(Math.random() * 4);
+  document.getElementById('find').innerHTML = find;
+  for (let i = 0; i < images.length; i++) {
+    images[i].setAttribute('onclick', 'imageCheck(this)');
     
+  }
+  document.getElementById('submit-button').addEventListener('click', submit)
+}
+const validate = (e) => {
+  valid = true;
+  //ALWAYS SELECTING FIRST LABEL
+  let inputs = document.querySelectorAll(".yoke");
+  if(document.getElementById('field-3').checked == true){checkDoesPass = true;}
+
+const revealError = (box, errorMessage) =>{
+  let errorBox = box.nextElementSibling;
+  errorBox.style.opacity = 1;
+  errorBox.innerHTML = errorMessage;
+}
+
   inputs.forEach((element) => {
+    var str = document.getElementById('field-2').value;
+  var patt = new RegExp(/.+\@.+\..+/);
+  var res = patt.test(str);
+
     element.classList.remove("error");
-    if (element.value == "" || checkDoesPass == false) {
-        
+
+    if (element.value == "") {
+      revealError(element, 'Error!')
+      //element.nextElementSibling.innerHTML = 'Error!'
+
       element.classList.add("error");
-        console.log('Error at', {element})
+        console.log('Error at', element)
+      valid = false;
+    }else if(res == false){
+      console.log('Error at', {element})
+      revealError(document.getElementById('field-2'), 'Please Enter Valid Email!')
+      valid = false;}
+    if(checkDoesPass == false){
+      console.log('Error at', {element})
+      revealError(document.getElementById('field-3'), 'Please Check!')
+      valid = false;
+    } 
+    if(movedGenres.length == 0){
+      console.log('Error at', {element})
+      revealError(document.getElementById("genre-title"), 'Please Select!')
       valid = false;
     }
+    if(correctImage == false){
+      console.log('Error at', {element})
+      revealError(document.getElementById("image-title"), 'Try Again!')
+      valid = false;
+    }//else{ valid = true;}
   });
-  if (valid == false) {
+  /*if (valid == false) {
     e.preventDefault();
     return false;
   } else {
+    alert('validated')
     return true;
-  }
+  }*/
 };
 const moveGenre = (element) =>{
   console.log({genres});
@@ -79,6 +125,36 @@ const generateGenres = () => {
   }
   document.getElementById('genreSecond').value = movedGenres;
 }
+const imageCheck = (element) => {
+  const number = element.getAttribute('value');
+  document.getElementById('find').innerHTML = find;
+  if(number == find){
+    correctImage = true;
+  }else{
+    correctImage = false;
+  }
+  for (let i = 0; i < images.length; i++) {
+    images[i].classList.add('invalid') 
+  }
+  element.classList.remove('invalid');
+  console.log(correctImage)
+  }
+
+const submit = () => {
+  validate()
+  if(valid == true){
+    url = ''
+  url +=  'https://webdevcit.com/displayvalues.php?';
+  url += 'name=' + escape(document.getElementById('field-1').value)
+  url += '&email=' + escape(document.getElementById('field-2').value); 
+  url += '&data=' + escape(checkDoesPass);
+  url += '&superhero=' + escape(movedGenres.join(','));
+  url += '&captca=' + escape(correctImage)
+
+  location = url;
+  }
+}
 
 
-init()
+
+init();
